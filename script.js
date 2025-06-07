17494203963e148ffe01e8cc23bc004a5a282cc2
supachaiB <96191074+supachaiB@users.noreply.github.com>
2025-06-07 15:17:00 +0700

    Initial commit

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();  // ป้องกันการกระโดดทันที
-      const targetID = this.getAttribute('href').substring(1);
-      const targetSection = document.getElementById(targetID);
-      
-      if (targetSection) {
-        window.scrollTo({
-          top: targetSection.offsetTop - 60, // ลบความสูง header ถ้ามี fixed header
-          behavior: 'smooth'
-        });
-      }
-    });
-  });
+// scroll to top
+const scrollTotop = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll",function(){
+  if (this.window.pageYOffset>300) {
+    scrollTotop.style.display="block";
+  }else{
+    
+    scrollTotop.style.display="none";
+  }
+});
+
+scrollTotop.addEventListener("click",function(){
+  window.scrollTo({
+    top:0,
+    behavior: "smooth"
+  })
+})
